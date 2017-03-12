04768e5a48cb1975a2c18b6ea10f7298744cabf2
jchrisvalmonte <jchrisvalmonte@gmail.com>
2017-03-12 01:59:28 -0800

    Added BS scrollspy. Added workshop details.

--- index.js
+++ index.js
@@ -1,30 +1,67 @@
 $(document).ready(function() {
 
-	$navbar = $('#navbar');
-	$navbar
-		.affix({
-			offset: {
-				top: $navbar.height(),
-				bottom: function(){
-					return (this.bottom = $('footer').outerHeight(true))
-				}
-			}
-		})
-		.on('', function() {
+	var $body = $('body');
+	$body.scrollspy({
+		target: '#navbar',
+		offset: 300,
+	});
 
-		})
-		.on('', function() {
+	var $navbar = $('#navbar');
+	$navbar.affix({ offset: { top: $navbar.height() } });
 
-		});
+	var $navLink = $navbar.find('.nav a');
+	$navLink.on('click', function() {
+		$('.navbar-collapse.in').collapse('hide');
+	});
+
+
+	var $workshops = $('main').find('.workshop');
+	$workshops.on('click', workshopDetailsPrepare);
+
+	var $workshopDetails = $('#workshop-details');
+	$workshopDetails.on('hidden.bs.modal', workshopDetailsClear);
 
 });
 
-function openNav() {
-	$menu = $('#menu');
-	$menu.attr('style', 'height: 100%;')
+
+
+function workshopDetailsClear ()
+{
+	var $workshopDetails = $(this);
+	$workshopDetails.find('.description').text('');
+	$workshopDetails.find('.location').text('');
+	$workshopDetails.find('.num').text('');
+	$workshopDetails.find('.name').text('');
+	$workshopDetails.find('.pillar').text('');
+	$workshopDetails.find('.presenters').text('');
 }
 
-function closeNav() {
-	$menu = $('#menu');
-	$menu.attr('style', 'height: 0;')
+
+
+function workshopDetailsPrepare ()
+{
+	var $workshopClone = $(this).clone(true, true);
+	var description 	= $workshopClone.find('.description').text();
+	var location 			= $workshopClone.find('.location').text();
+	var num 					= $workshopClone.find('.num').text();
+	var name 					= $workshopClone.find('.name').text();
+	var pillar 				= $workshopClone.find('.pillar').text();
+	var presenters	 	= $workshopClone.find('.presenters').text();
+
+
+	var $workshopDetails = $('#workshop-details');
+	var $wdDescription 	= $workshopDetails.find('.description');
+	var $wdLocation 		= $workshopDetails.find('.location');
+	var $wdNum 					= $workshopDetails.find('.num');
+	var $wdName 				= $workshopDetails.find('.name');
+	var $wdPillar 			= $workshopDetails.find('.pillar');
+	var $wdPresenters 	= $workshopDetails.find('.presenters');
+
+
+	$wdDescription.append(description);
+	$wdLocation.append(location);
+	$wdNum.append(num);
+	$wdName.append(name);
+	$wdPillar.append(pillar);
+	$wdPresenters.append(presenters);
 }
